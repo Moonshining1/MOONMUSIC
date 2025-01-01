539a8e24a095cc5552b2180e9840137f98d2e230
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2025-01-01 22:24:55 +0530

    Update Soundcloud.py

--- MOON_MUSIC/platforms/Soundcloud.py
+++ MOON_MUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from SACHIN_MUSIC.utils.formatters import seconds_to_min
+from MOON_MUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
