0ba9d497a7dcd84ea33e515c4c93b56e73800fe0
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2025-01-01 22:58:03 +0530

    Update start.py

--- MOON_MUSIC/plugins/bot/start.py
+++ MOON_MUSIC/plugins/bot/start.py
@@ -3,13 +3,14 @@ import random
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from pyrogram.errors import ButtonUserPrivacyRestricted, BadRequest
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from MOON_MUSIC import app
 from MOON_MUSIC.misc import _boot_
-from MOON_MUSIC.utils.decorators.language import LanguageStart
+from MOON_MUSIC.plugins.sudo.sudoers import sudoers_list
+from MOON_MUSIC.utils.database import get_served_chats, get_served_users, get_sudoers
+from MOON_MUSIC.utils import bot_sys_stats
 from MOON_MUSIC.utils.database import (
     add_served_chat,
     add_served_user,
@@ -18,6 +19,7 @@ from MOON_MUSIC.utils.database import (
     is_banned_user,
     is_on_off,
 )
+from MOON_MUSIC.utils.decorators.language import LanguageStart
 from MOON_MUSIC.utils.formatters import get_readable_time
 from MOON_MUSIC.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
@@ -57,50 +59,83 @@ HIMANSHI = [
 ]
 
 
-
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
-        if name.startswith("help"):
+        if name[0:4] == "help":
             keyboard = help_pannel(_)
-            try:
-                await message.reply_sticker(random.choice(HIMANSHI))
-                await message.reply_photo(
-                    random.choice(NEXIO),
-                    caption=_["help_1"].format(config.SUPPORT_CHAT),
-                    reply_markup=keyboard,
+            await message.reply_sticker(
+            random.choice(HIMANSHI),)
+            return await message.reply_photo(
+                random.choice(NEXIO),
+                caption=_["help_1"].format(config.SUPPORT_CHAT),
+                reply_markup=keyboard,
+            )
+        if name[0:3] == "sud":
+            await sudoers_list(client=client, message=message, _=_)
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>✦ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>✦ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
                 )
-            except ButtonUserPrivacyRestricted:
-                await message.reply_text(
-                    "Your privacy settings restrict interaction with inline buttons."
+            return
+        if name[0:3] == "inf":
+            m = await message.reply_text("🔎")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+            searched_text = _["start_6"].format(
+                title, duration, views, published, channellink, channel, app.mention
+            )
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=_["S_B_8"], url=link),
+                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+
+chat_id=message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                reply_markup=key,
+            )
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
                 )
-            return
-        elif name.startswith("sud"):
-            # Handle sudo logic here
-            return
-        elif name.startswith("inf"):
-            # Handle info logic here
-            return
     else:
         out = private_panel(_)
         served_chats = len(await get_served_chats())
         served_users = len(await get_served_users())
         UP, CPU, RAM, DISK = await bot_sys_stats()
-        try:
-            await message.reply_sticker(random.choice(HIMANSHI))
-            await message.reply_photo(
-                random.choice(NEXIO),
-                caption=_["start_2"].format(
-                    message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats
-                ),
-                reply_markup=InlineKeyboardMarkup(out),
-            )
-        except ButtonUserPrivacyRestricted:
-            await message.reply_text(
-                "Your privacy settings restrict interaction with inline buttons."
+        await message.reply_sticker(
+        random.choice(HIMANSHI),)
+        await message.reply_photo(
+            random.choice(NEXIO),
+            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
+        if await is_on_off(2):
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
             )
 
 
@@ -109,18 +144,14 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    try:
-        await message.reply_sticker(random.choice(HIMANSHI))
-        await message.reply_photo(
-            random.choice(NEXIO),
-            caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
-            reply_markup=InlineKeyboardMarkup(out),
-        )
-    except ButtonUserPrivacyRestricted:
-        await message.reply_text(
-            "Your privacy settings restrict interaction with inline buttons."
-        )
-    await add_served_chat(message.chat.id)
+    await message.reply_sticker(
+    random.choice(HIMANSHI),)
+    await message.reply_photo(
+        random.choice(NEXIO),
+        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
+        reply_markup=InlineKeyboardMarkup(out),
+    )
+    return await add_served_chat(message.chat.id)
 
 
 @app.on_message(filters.new_chat_members, group=-1)
@@ -132,7 +163,7 @@ async def welcome(client, message: Message):
             if await is_banned_user(member.id):
                 try:
                     await message.chat.ban_member(member.id)
-                except BadRequest:
+                except:
                     pass
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
@@ -148,23 +179,21 @@ async def welcome(client, message: Message):
                         disable_web_page_preview=True,
                     )
                     return await app.leave_chat(message.chat.id)
+
                 out = start_panel(_)
-                try:
-                    await message.reply_sticker(random.choice(HIMANSHI))
-                    await message.reply_photo(
-                        random.choice(NEXIO),
-                        caption=_["start_3"].format(
-                            message.from_user.mention,
-                            app.mention,
-                            message.chat.title,
-                            app.mention,
-                        ),
-                        reply_markup=InlineKeyboardMarkup(out),
-                    )
-                except ButtonUserPrivacyRestricted:
-                    await message.reply_text(
-                        "Your privacy settings restrict interaction with inline buttons."
-                    )
+                await message.reply_sticker(
+                random.choice(HIMANSHI),)
+                await message.reply_photo(
+                    random.choice(NEXIO),
+                    caption=_["start_3"].format(
+                        message.from_user.mention,
+                        app.mention,
+                        message.chat.title,
+                        app.mention,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
                 await add_served_chat(message.chat.id)
+                await message.stop_propagation()
         except Exception as ex:
-            print(f"Error: {ex}")
+            print(ex)
