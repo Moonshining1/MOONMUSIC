5d0baf3173ee7d1559832bcf32ec55c511c00d3c
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2025-01-06 17:56:11 +0530

    Update settings.py

--- MOON_MUSIC/plugins/bot/settings.py
+++ MOON_MUSIC/plugins/bot/settings.py
@@ -1,7 +1,12 @@
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageNotModified
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 from MOON_MUSIC import app
 from MOON_MUSIC.utils.database import (
@@ -22,12 +27,19 @@ from MOON_MUSIC.utils.database import (
 )
 from MOON_MUSIC.utils.decorators.admins import ActualAdminCB
 from MOON_MUSIC.utils.decorators.language import language, languageCB
-from MOON_MUSIC.utils.inline.settings import auth_users_markup, playmode_users_markup, setting_markup, vote_mode_markup
+from MOON_MUSIC.utils.inline.settings import (
+    auth_users_markup,
+    playmode_users_markup,
+    setting_markup,
+    vote_mode_markup,
+)
 from MOON_MUSIC.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
-@app.on_message(filters.command(["settings", "setting"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(["settings", "setting"]) & filters.group & ~BANNED_USERS
+)
 @language
 async def settings_mar(client, message: Message, _):
     buttons = setting_markup(_)
@@ -45,17 +57,14 @@ async def settings_cb(client, CallbackQuery, _):
     except:
         pass
     buttons = setting_markup(_)
-    try:
-        await CallbackQuery.edit_message_text(
-            _["setting_1"].format(
-                app.mention,
-                CallbackQuery.message.chat.id,
-                CallbackQuery.message.chat.title,
-            ),
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
-    except MessageNotModified:
-        pass
+    return await CallbackQuery.edit_message_text(
+        _["setting_1"].format(
+            app.mention,
+            CallbackQuery.message.chat.id,
+            CallbackQuery.message.chat.title,
+        ),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
 
 
 @app.on_callback_query(filters.regex("settingsback_helper") & ~BANNED_USERS)
@@ -69,21 +78,16 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
         await app.resolve_peer(OWNER_ID)
         OWNER = OWNER_ID
         buttons = private_panel(_)
-        try:
-            await CallbackQuery.edit_message_text(
-                _["start_2"].format(CallbackQuery.from_user.mention, app.mention),
-                reply_markup=InlineKeyboardMarkup(buttons),
-            )
-        except MessageNotModified:
-            pass
+        return await CallbackQuery.edit_message_text(
+            _["start_2"].format(CallbackQuery.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
     else:
         buttons = setting_markup(_)
-        try:
-            await CallbackQuery.edit_message_reply_markup(
-                reply_markup=InlineKeyboardMarkup(buttons)
-            )
-        except MessageNotModified:
-            pass
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+
 
 @app.on_callback_query(
     filters.regex(
